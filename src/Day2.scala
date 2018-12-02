6518e7297090fc1514d4a259aeb095294b6b80a4
Kaspar Helldén <kaspar@hellden.com>
2018-12-02 16:00:38 +0100

    day 2

--- src/Day2.scala
+++ src/Day2.scala
@@ -4,4 +4,16 @@ object Day2 extends App {
 
   val input = Source.fromFile("input/day2.txt").getLines().toList
 
+  val lengths = input.map(_.groupBy(identity).map(_._2.length).toSet)
+  val product = lengths.count(_.contains(2)) * lengths.count(_.contains(3))
+  println(product) // 7470
+
+  val offByOne = input.view.flatMap {
+    code =>
+      input
+        .view
+        .map(_.zip(code).collect { case (c1, c2) if c1 == c2 => c1 }.mkString)
+        .find(_.length == code.length - 1)
+  }.head
+  println(offByOne) // kqzxdenujwcstybmgvyiofrrd
 }
