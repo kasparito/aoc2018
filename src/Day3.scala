f9a387e045b1db0d15eb092cc340928571c753b6
Kaspar Helldén <kaspar@hellden.com>
2018-12-03 10:00:46 +0100

    cleanup

--- src/Day3.scala
+++ src/Day3.scala
@@ -4,31 +4,19 @@ object Day3 extends App {
 
   val input = Source.fromFile("input/day3.txt").getLines().toList
 
-  val ClaimPattern = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".r
-  case class Claim(id: Int, dx: Int, dy: Int, w: Int, h: Int) {
-    val x = Range(dx, dx + w)
-    val y = Range(dy, dy + h)
-  }
-  val claims = input.view.map {
-    case ClaimPattern(id, dx, dy, w, h) =>
-      Claim(id.toInt, dx.toInt, dy.toInt, w.toInt, h.toInt)
-  }.toList
-  val coordinates = for {
-    claim <- claims
-    x <- claim.x
-    y <- claim.y
-  } yield ((x, y), claim)
-  val coordinateClaims = coordinates
-    .groupBy(_._1)
-    .mapValues(_.map(_._2))
-    .view.force
+  val Claim = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".r
+
+  val coordinateClaims = {
+    for {
+      Claim(id, dx, dy, w, h) <- input
+      x <- Range(dx.toInt, dx.toInt + w.toInt)
+      y <- Range(dy.toInt, dy.toInt + h.toInt)
+    } yield ((x, y), id.toInt)
+  }.groupBy(_._1).mapValues(_.map(_._2).toSet).view.force
 
   println(coordinateClaims.values.count(_.size > 1)) // 118539
 
-  val idCounts = coordinateClaims.values
-    .flatMap(claims => claims.map(_.id -> claims.size))
-    .groupBy(_._1)
-    .mapValues(_.map(_._2).toSet)
-    .view.force
-  println(idCounts.find(_._2 == Set(1)).head._1) // 1270
+  val claimCounts = coordinateClaims.values.flatMap(claims => claims.map(_ -> claims.size))
+    .groupBy(_._1).mapValues(_.map(_._2).toSet).view.force
+  println(claimCounts.find(_._2 == Set(1)).head._1) // 1270
 }
---
+++ src/Day4.scala
@@ -0,0 +1,7 @@
+import scala.io.Source
+
+object Day4 extends App {
+
+  val input = Source.fromFile("input/day4.txt").getLines().toList
+
+}
