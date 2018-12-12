2fc50c40e423c6be216d30d53b5c0f5a5964382d
Kaspar Helldén <kaspar@hellden.com>
2018-12-12 09:44:14 +0100

    day 12 part 2

--- src/Day12.scala
+++ src/Day12.scala
@@ -1,22 +1,52 @@
 object Day12 extends Base(12) {
 
-  val generations = 20
-  val initial = Array.fill(generations)('.').mkString + inputLines.head.substring("initial state: ".length) + Array.fill(generations)('.').mkString
+  val initial = inputLines.head.substring("initial state: ".length)
 
   val Note = """([.#]{5}) => ([.#])""".r
   val notes = inputLines.tail.tail
     .map { case Note(pattern, result) => pattern -> result }
     .toMap
 
-  override def part1 = // 3276
-    (0 until generations).foldLeft(initial) {
-      case (state, generation) =>
-        println(s"$generation: $state")
-        (".." + state + "..").sliding(5).map(notes.getOrElse(_, ".")).mkString
-    }.zipWithIndex.collect {
+  def sumPotIndexes(generations: Long) = {
+    var generation = 0L
+    var state = initial
+    var offset = 0L
+
+    println(s"$generation ($offset): $state")
+
+    do {
+      val firstIndex = state.indexOf('#')
+      val padded = "...." + state.substring(firstIndex, state.lastIndexOf('#') + 1) + "...."
+
+      generation += 1
+      state = padded.sliding(5).map(notes.getOrElse(_, ".")).mkString
+      offset = offset - 2 + firstIndex
+
+      println(s"$generation ($offset): $state")
+
+    } while (generation < generations)
+
+    state.zipWithIndex.collect {
       case ('#', index) =>
-        index - generations
+        offset + index
     }.sum
+  }
 
+  private def padding(index: Int) =
+    if (index > 5)
+      ""
+    else
+      Array.fill(5 - index)('.').mkString
 
+  override def part1 = // 3276
+    sumPotIndexes(20)
+
+  override def part2 = { //
+    val state = "...###.....###....###...###.....###....###.....###.....###.....###...###.....###....###....###.....###....###....###.....###....###...###...###...###...###...###....###...###."
+    val offset = 50000000000L - 76
+    state.zipWithIndex.collect {
+      case ('#', index) =>
+        offset + index
+    }.sum
+  }
 }
